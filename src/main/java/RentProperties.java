5e85d085aea6cd55ea4b619c840a0bcdaae9ea71
Artem Grigorov <artem_grigorov@mail.ru>
2016-04-24 16:40:04 +0300

    Gtgnore & RentProperties was added

---
+++ src/main/java/RentProperties.java
@@ -0,0 +1,34 @@
+/**
+ * Created by User on 23.04.16.
+ */
+public class RentProperties {
+
+    private static String priceFrom;
+    private static String priceTo;
+    private static int district;
+
+    public static String getPriceTo() {
+        return priceTo;
+    }
+
+    public static void setPriceTo(String priceTo) {
+        RentProperties.priceTo = priceTo;
+    }
+
+    public static int getDistrict() {
+        return district;
+    }
+
+    public static void setDistrict(int district) {
+        RentProperties.district = district;
+    }
+
+    public static String getPriceFrom() {
+
+        return priceFrom;
+    }
+
+    public static void setPriceFrom(String priceFrom) {
+        RentProperties.priceFrom = priceFrom;
+    }
+}
